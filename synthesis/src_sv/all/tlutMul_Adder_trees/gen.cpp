a798860fce4e84ebaf73fe2f85993c94f44fbc74
Aishwarya Lekshmi Chithra <chithra@elab-vulcan.ece.wisc.edu>
2021-11-30 17:13:21 -0600

    Adding final c file to take care of error conditions

--- synthesis/src_sv/all/tlutMul_Adder_trees/gen.cpp
+++ synthesis/src_sv/all/tlutMul_Adder_trees/gen.cpp
@@ -14,15 +14,28 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int dim=int(strtol(argv[1],NULL,10));
+    if (argc < 5) {
+        cout << "[ERROR] Not enough arguments. Enter as ./test <ROW1> <COL1> <ROW2> <COL2>" << endl;
+        return -1;
+    }
 
-    for (int i = 0; i < dim; i++) {
-      for (int j = 0; j < dim; j++) {
+    int row1=int(strtol(argv[1],NULL,10));
+    int col1=int(strtol(argv[2],NULL,10));
+    int row2=int(strtol(argv[3],NULL,10));
+    int col2=int(strtol(argv[4],NULL,10));
+
+    if (col1!=row2) {
+        cout << "[ERROR] The two matrices are not compatible for Multiplication" << endl;
+        return -1;
+    }
+
+    for (int i = 0; i < row1; i++) {
+      for (int j = 0; j < col2; j++) {
             //cout << "mul_temp[" << i <<"][" << j << "] = ";
-            cout << "mul_temp[" << i*dim+j << "] = ";
-            for (int k = 0; k < dim; k++) {
-                cout << "prod[" << i*dim+k << "][" <<k*dim+j <<"]";
-                if (k!=dim-1) cout <<" + ";
+            cout << "mul_temp[" << i*row1+j << "] = ";
+            for (int k = 0; k < col1; k++) {
+                cout << "prod[" << i*col1+k << "][" <<k*col2+j <<"]";
+                if (k!=col1-1) cout <<" + ";
         }
         cout << ";"<<endl;
         }
