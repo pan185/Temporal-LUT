b0245ea7a9cfe882ac6b032913e3dd0c7ff388ca
prajyotgupta <prajyotgupta@gmail.com>
2021-12-04 17:31:01 -0600

    Added scripts for extracting & analysis for MNIST inputs

--- benchmarks/mnist_inputs/scripts/display.py
+++ benchmarks/mnist_inputs/scripts/display.py
@@ -7,7 +7,8 @@
 import idx2numpy
 import numpy as np
 import matplotlib.pyplot as plt
-file = 'train-images-idx3-ubyte'
+file = '../data/train-images-idx3-ubyte'
 arr = idx2numpy.convert_from_file(file)
 # arr is now a np.ndarray type of object of shape 60000, 28, 28
+# Display image-57
 plt.imshow(arr[57], cmap=plt.cm.binary)
